bfc9dc93c5e7daf49f43d314f7c0031ac317963e
indhuthanneeru <indhuthanneiru@gmail.com>
2026-08-28 18:57:40 +0530

    Delete interrupts.d

--- interrupts.d
+++
@@ -1,5 +0,0 @@
-interrupts.o: ..\cpp\ARM_G7\MAJOR_PROJECT\interrupts.c
-interrupts.o: ..\cpp\ARM_G7\MAJOR_PROJECT\interrupts.h
-interrupts.o: C:\KeilARM\ARM\INC\Philips\lpc21xx.h
-interrupts.o: C:\KeilARM\ARM\RV31\INC\string.h
-interrupts.o: ..\cpp\ARM_G7\MAJOR_PROJECT\uart0.h
