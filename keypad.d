694b72f19841e7ba6e9d792c8df8e5c9487485c8
indhuthanneeru <indhuthanneiru@gmail.com>
2026-09-21 17:41:35 +0530

    Delete keypad.d

--- keypad.d
+++
@@ -1,5 +0,0 @@
-keypad.o: ..\cpp\ARM_G7\MAJOR_PROJECT\keypad.c
-keypad.o: C:\KeilARM\ARM\INC\Philips\lpc21xx.h
-keypad.o: ..\cpp\ARM_G7\MAJOR_PROJECT\delay.h
-keypad.o: ..\cpp\ARM_G7\MAJOR_PROJECT\keypad.h
-keypad.o: ..\cpp\ARM_G7\MAJOR_PROJECT\lcd.h
